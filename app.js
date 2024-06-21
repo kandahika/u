5de8a138e7e4b1fc5f92efd82a6692997cef337d
kandahika <166777356+kandahika@users.noreply.github.com>
2024-06-21 10:45:29 +0900

    uuu

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.get("/", (req, res) => res.type('html').send(html));
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
+
 server.headersTimeout = 120 * 1000;
 
 const html = `
